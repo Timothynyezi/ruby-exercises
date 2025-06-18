0182e07df772f24fc9285c1be75ef8507b154d9b
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-06-18 19:16:24 +0200

    wrote answers for the remove first three elements array function

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -30,9 +30,10 @@ end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
+  array.drop(3)
 
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
-  array
+  return array
 end
 
 def array_concatenation(original, additional)
