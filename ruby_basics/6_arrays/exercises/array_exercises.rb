dc5e82410640aef7f841a0751a05e38f35f759a1
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2025-06-18 19:11:07 +0200

    wrote a few solutions for the array_exercise

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -4,10 +4,12 @@ end
 
 def first_element(array)
   # return the first element of the array
+  return array.unshift
 end
 
 def third_element(array)
   # return the third element of the array
+  return array[2]
 end
 
 def last_three_elements(array)
@@ -20,9 +22,10 @@ end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
+  array.pop
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  array
+  return array
 end
 
 def remove_first_three_elements(array)
@@ -42,6 +45,7 @@ end
 
 def empty_array?(array)
   # return true if the array is empty
+  return array.empty?
 end
 
 def reverse(array)
@@ -50,10 +54,13 @@ end
 
 def array_length(array)
   # return the length of the array
+  return array.length
 end
 
 def include?(array, value)
   # return true if the array includes the value
+  return array.include?(value)
+
 end
 
 def join(array, separator)
